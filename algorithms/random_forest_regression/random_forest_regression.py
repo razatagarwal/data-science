a2f12ecff402e110c8b454343702780dc2f4a41c
Razat Agarwal <razatagarwal98@gmail.com>
2019-04-12 22:11:58 +0530

    Conversion to 2d arraywq

--- algorithms/random_forest_regression/random_forest_regression.py
+++ algorithms/random_forest_regression/random_forest_regression.py
@@ -14,7 +14,8 @@ regressor = RandomForestRegressor(n_estimators = 10,
                                   random_state = 0)
 regressor.fit(x, y)
 
-y_pred = regressor.predict(6.5)
+y_pred = regressor.predict(np.array([[6.5]]))
+print("For position 6.5, Salary = ", y_pred)
 
 X_grid = np.arange(min(x), max(x), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
