479f0b69adc9921e180b95770a2f28e584266d69
Razat Agarwal <razatagarwal98@gmail.com>
2019-04-12 22:13:16 +0530

    Change in library import

--- algorithms/logistic_regression/logistic_regression.py
+++ algorithms/logistic_regression/logistic_regression.py
@@ -9,7 +9,7 @@ dataset = pd.read_csv('dataset/Social_Network_Ads.csv')
 x = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, 
                                                     y, 
                                                     test_size = 0.25, 
